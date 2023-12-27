80f27944103c3c9fd00d4843ba93aff6df7692f0
Jimmy Hooker <james.hooker@fivetran.com>
2023-12-27 16:58:11 -0700

    removing reference to jaffle_shop

--- jaffle_shop/models/google_sheet_cloud/bachelor.sql
+++ jaffle_shop/models/google_sheet_cloud/bachelor.sql
@@ -1,9 +1,5 @@
 
 -- Use the `ref` function to select from other models
 
--- select *
--- from {{ ref('my_first_dbt_model') }}
--- where id = 1
-
 select * from `fivetran-fivetran-jimmy-d-vk-8.google_sheets.dbt_cloud_sample` 
 where education = 'Bachelor'
